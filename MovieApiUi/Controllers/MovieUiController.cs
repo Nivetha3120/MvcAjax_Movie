38f5c48feea903b5fc08184a70b1cb04ae04d938
Nivetha Natarajan <nivethan@caresoftglobal.com>
2022-02-09 19:27:42 +0530

    uploaded successfully

---
+++ MovieApiUi/Controllers/MovieUiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApiUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace MovieApiUI.Controllers
+{
+    public class MovieUiController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+    
+    }
+}
---
+++ MovieApiUi/Models/dataprop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApiUI.Models
+{
+    public class dataprop
+    {
+        
+            [Key]
+            public int movieid { get; set; }
+            public string moviename { get; set; }
+            public string language { get; set; }
+            public string genre { get; set; }
+        
+    }
+}
